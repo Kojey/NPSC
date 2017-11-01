6e9c5bb2f081dbe769dd8d1f66f51ed3195fd8e3
Kojey <kylekojey@gmail.com>
2017-11-02 01:04:57 +0200

    software-arduino: Add arduino code to obtain board temperature

---
+++ software/code/arduino/board_temperature/board_temperature.ino
@@ -0,0 +1,46 @@
+/**
+ * @brief: Get average temperature of the board using four temperature sensors
+ * @author: Othniel Konan
+ * @version: v1.0
+ * @date: 2 Nov 2017
+ */
+int temperatureSensor[4] = {A0, A1,A2,A3}; // select the input pin for the potentiometer
+int samplingInterval = 2000;
+int sampleNumber = 0;
+int numberOfSamples = 15;
+int averageTemperature = 0;  // variable to store the value coming from the sensor
+int previousTemperature = 10000;
+int difference = 10;
+void setup() {
+  Serial.begin(9600);
+  delay(samplingInterval);
+  
+  Serial.println("Current drawn: X A");
+  Serial.println("Sample,T1,T2,T3,T4,Tavg");
+}
+
+void loop() {
+ while(abs(averageTemperature-previousTemperature)>=difference){
+    previousTemperature = averageTemperature;
+    ++sampleNumber;
+    averageTemperature = 0; // reset average
+    // read data from all sensor and find the average
+    Serial.print(sampleNumber);
+    Serial.print(',');
+    for(int i=0; i<4; ++i){
+      int value = getTemperature(analogRead(temperatureSensor[i]));
+      Serial.print(value);
+      Serial.print(',');
+      averageTemperature += value;
+    }
+    averageTemperature /= 4;
+    Serial.println(averageTemperature);
+    // delay to next sample
+    delay(samplingInterval);
+  }
+}
+
+int getTemperature(int temp){
+  return temp;
+}
+
--- software/code/arduino/neopixel/examples/simple/simple.ino
+++ software/code/arduino/neopixel/examples/simple/simple.ino
@@ -11,7 +11,7 @@
 #define PIN            6
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      16
+#define NUMPIXELS      180
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
@@ -37,11 +37,11 @@ void loop() {
   for(int i=0;i<NUMPIXELS;i++){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(0,15,0)); // Moderately bright green color.
 
     pixels.show(); // This sends the updated pixel color to the hardware.
 
-    delay(delayval); // Delay for a period of time (in milliseconds).
+   // delay(delayval); // Delay for a period of time (in milliseconds).
 
   }
 }
